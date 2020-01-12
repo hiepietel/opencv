a29b0c3e849e1a351bcea295dc9cd9ecc643a461
jachumail <jachumail@yahoo.pl>
2020-01-12 23:43:07 +0100

    facedetection

--- cv_tut6_facedetection.py
+++ cv_tut6_facedetection.py
@@ -2,12 +2,22 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('image.jpg')
+#img = cv2.imread('image.jpg')
+cap = cv2.VideoCapture(0) 
+while cap.isOpened():
+    _, img = cap.read()
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-cv2.imshow('img', img)
+    for(x, y, w, h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 3)
+    cv2.imshow('img', img)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
 
-
-cv2.waitKey()
+cap.release()
 cv2.destroyAllWindows()
 
 
