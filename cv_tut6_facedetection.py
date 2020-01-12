b77e98a8f56497eccf1f9f9f318c685e9ba45887
jachumail <jachumail@yahoo.pl>
2020-01-12 23:54:09 +0100

    eye detection

--- cv_tut6_facedetection.py
+++ cv_tut6_facedetection.py
@@ -1,23 +1,31 @@
 import cv2 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
-#img = cv2.imread('image.jpg')
-cap = cv2.VideoCapture(0) 
-while cap.isOpened():
-    _, img = cap.read()
+img = cv2.imread('image.jpg')
+#cap = cv2.VideoCapture(0) 
+#while cap.isOpened():
+    #_, img = cap.read()
     
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    for(x, y, w, h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 3)
-    cv2.imshow('img', img)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+for(x, y, w, h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 3)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for(ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex,ey),(ex+ew, ey+eh), (0,255,0), 5)
 
-cap.release()
+cv2.imshow('img', img)
+#key = cv2.waitKey(1)
+#if key == 27:
+#    break
+
+#cap.release()
+cv2.waitKey()
 cv2.destroyAllWindows()
 
 
